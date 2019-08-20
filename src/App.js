573722aeb5061cbb6f2f3362a8bdda6e77a749d5
Chinseok Lee <me@askcompany.kr>
2019-08-20 15:39:22 +0900

    axios를 통해서 localhost:8000/univ/professor.json 으로부터 Professor List 로딩

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import Axios from 'axios';
 import './App.css';
 import ProfessorList from "./pages/professor_list";
 
+// 아래 함수는 매 render 시마다 호출이 됩니다.
 function App() {
-    const professorList = [
-        { name: '이진석', phone: '01012341234' },
-        { name: '김딘딘', phone: '01045631023' },
-        { name: '박갑돌', phone: '01009874514' }
-    ];
+    // 리액트 컴포넌트에서 UI 관련된 2가지 종류의 값
+    //   - 속성값 : props => 부모로 부터 받는 값들
+    //   - 상탯값 : state => 현재 컴포넌트가 직접 생성/관리하는 값들
 
+    const [state, setState] = useState({ professorList: [] });
+
+    // - intpus: 안 쓰면     => 매 render 완료시마다 호출
+    // - inputs: []         =>  컴포넌트가 생성될 때, 1회만 호출
+    // - inputs: [어떤상탯값] => 그 상태값이 변경될 때마다, 호출
+    useEffect(() => {
+        const fetchList = async () => {
+            const url = "http://localhost:8000/univ/professor.json";
+            const { data } = await Axios.get(url);
+            console.log(data);
+            setState({
+                professorList: data
+            });
+        };
+        fetchList();
+    }, []);
+
+    const { professorList } = state;
     return (
         <div>
             Hello React
