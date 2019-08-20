711631ddb027b3119c62bb6f9cdfcaac8a77bb16
Chinseok Lee <me@askcompany.kr>
2019-08-20 16:55:43 +0900

    검색어 query를 기반으로 professorList를 필터링하여, filteredProfessorLIst를 적용

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useMemo, useState} from 'react';
 import Axios from 'axios';
 import './App.css';
 import ProfessorList from "./pages/professor_list";
@@ -9,7 +9,12 @@ function App() {
     //   - 속성값 : props => 부모로 부터 받는 값들
     //   - 상탯값 : state => 현재 컴포넌트가 직접 생성/관리하는 값들
 
-    const [state, setState] = useState({ professorList: [] });
+    const [state, setState] = useState({
+        query: '',
+        professorList: []
+    });
+
+    const { query, professorList } = state;
 
     // - intpus: 안 쓰면     => 매 render 완료시마다 호출
     // - inputs: []         =>  컴포넌트가 생성될 때, 1회만 호출
@@ -20,17 +25,44 @@ function App() {
             const { data } = await Axios.get(url);
             console.log(data);
             setState({
+                ...state,  // 클래스형 컴포넌트에서는 불필요
                 professorList: data
             });
         };
         fetchList();
     }, []);
 
-    const { professorList } = state;
+    // 상탯값 query가 변경이 되면,
+    // professorList를 필터링해서, 새로운 filteredProfessorList 배열을 만들겠다.
+    const filteredProfessorList = useMemo(() => {
+        return professorList.filter(professor =>
+            (query.length === 0) ||
+                (professor.name.indexOf(query) > -1)
+        )
+    }, [query, professorList]);
+
+    const onChange = e => {
+        // const query = e.target.value;
+        const { value } = e.target;
+        console.log(value);
+        setState({
+            ...state,  // 클래스형 컴포넌트에서는 불필요.
+            query: value
+        });
+    };
+
     return (
         <div>
             Hello React
-            <ProfessorList professorList={professorList} />
+            <hr />
+
+            <input type="text" onChange={onChange} />
+
+            <hr />
+
+            <ProfessorList professorList={filteredProfessorList} />
+            <hr/>
+            현재 상탯값 : {JSON.stringify(state)}
         </div>
     );
 }
