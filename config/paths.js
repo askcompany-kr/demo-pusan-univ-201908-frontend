081639d12e58af98363d6d12448327be7de81c0a
Chinseok Lee <me@askcompany.kr>
2019-08-21 19:03:43 +0900

    배포 빌드 설정 추가: yarn build 내역을 ../backend/assets 경로에 저장합니다.

--- config/paths.js
+++ config/paths.js
@@ -25,18 +25,6 @@ function ensureSlash(inputPath, needsSlash) {
 const getPublicUrl = appPackageJson =>
   envPublicUrl || require(appPackageJson).homepage;
 
-// We use `PUBLIC_URL` environment variable or "homepage" field to infer
-// "public path" at which the app is served.
-// Webpack needs to know it to put the right <script> hrefs into HTML even in
-// single-page apps that may serve index.html for nested URLs like /todos/42.
-// We can't use a relative path in HTML because we don't want to load something
-// like /todos/42/static/js/bundle.7289d.js. We have to know the root.
-function getServedPath(appPackageJson) {
-  const publicUrl = getPublicUrl(appPackageJson);
-  const servedUrl =
-    envPublicUrl || (publicUrl ? url.parse(publicUrl).pathname : '/');
-  return ensureSlash(servedUrl, true);
-}
 
 const moduleFileExtensions = [
   'web.mjs',
@@ -69,7 +57,6 @@ const resolveModule = (resolveFn, filePath) => {
 module.exports = {
   dotenv: resolveApp('.env'),
   appPath: resolveApp('.'),
-  appBuild: resolveApp('build'),
   appPublic: resolveApp('public'),
   appHtml: resolveApp('public/index.html'),
   appIndexJs: resolveModule(resolveApp, 'src/index'),
@@ -82,11 +69,13 @@ module.exports = {
   proxySetup: resolveApp('src/setupProxy.js'),
   appNodeModules: resolveApp('node_modules'),
   publicUrl: getPublicUrl(resolveApp('package.json')),
-  servedPath: getServedPath(resolveApp('package.json')),
 
   bundlePublicPath: 'http://localhost:3000/',
   statsDirPath: resolveApp('../backend'),
   statsFilename: 'webpack-stats.json',
+
+  appBuild: resolveApp('../backend/assets'),
+  servedPath: 'static',
 };
 
 
