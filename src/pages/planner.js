288f522597a035862bfe30699a9abd80bca801b2
Chinseok Lee <me@askcompany.kr>
2019-08-21 14:33:26 +0900

    ProfessorListPage를 antd에 맞춰서 구현

---
+++ src/pages/planner.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PlannerPage = () => {
+    return (
+        <div>
+            교수 계획 페이지
+        </div>
+    );
+};
+
+export default PlannerPage;
--- src/pages/professor_list.js
+++ src/pages/professor_list.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import Axios from "axios";
+import {PageHeader, Table} from "antd";
+
+const columns = [
+    { title: '이름', dataIndex: 'name', key: 'name' },
+    { title: '전화번호', dataIndex: 'phone', key: 'phone' }
+];
 
 const ProfessorListPage = () => {
+    // const [state, setState] = useState({professorList: []});
+    const [professorList, setProfessorList] = useState([]);
+
+    useEffect(() => {
+        const fetchList = async () => {
+            const url = "http://localhost:8000/univ/professor.json";
+            const {data} = await Axios.get(url);
+            setProfessorList(data);
+        };
+        fetchList();
+    }, []);
+
     return (
         <div>
-            교수목록
+            <PageHeader title="교수목록" subTitle="subTitle" />
+
+            <Table columns={columns} dataSource={professorList}
+                   pagination={false}
+            />
         </div>
     );
 };
